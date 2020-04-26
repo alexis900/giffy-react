d68c24f336df8b4b3ff23eef71467242cc701449
Alejandro Martin <alexandermpf77@gmail.com>
2020-04-26 17:31:05 +0200

    Added Routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./App.css";
 import ListOfGifs from "./components/ListOfGifs";
 
+import { Route } from 'wouter'
+
 
 export default function App() {
   return (
     <div className="App">
       <section className="App-content">
-        <ListOfGifs keyword = 'ecuador'/>
+        <h1>App</h1>
+        <a>Gifs de Pandas</a>
+        <Route path="/search/:keyword" component={ListOfGifs}/>
       </section>
     </div>
   );
--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -2,13 +2,17 @@ import React, { useEffect, useState } from "react";
 import Gif from "./Gif";
 import getGifs from "../services/getGifs";
 
-export default function ListOfGifs({ keyword }) {
-    
+export default function ListOfGifs({ params }) {
+  const { keyword } = params
   const [gifs, setGifs] = useState([]);
   useEffect(() => {
     getGifs({ keyword }).then((gifs) => setGifs(gifs));
-  });
-  return gifs.map(({ id, title, url }) => (
-    <Gif id={id} key={id} title={title} url={url} />
-  ));
+  }, [keyword]);
+  return (
+    <>
+      {gifs.map(({ id, title, url }) => (
+        <Gif id={id} key={id} title={title} url={url} />
+      ))}
+    </>
+  );
 }
